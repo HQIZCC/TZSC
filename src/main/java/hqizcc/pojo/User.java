58346fb86156ae5a097e965c222e35473aa763b2
HQIZCC <1358805979@qq.com>
2017-10-23 21:08:48 +0800

    2017 / 10 /23 1.测试github

--- src/main/java/hqizcc/pojo/User.java
+++ src/main/java/hqizcc/pojo/User.java
@@ -3,6 +3,16 @@ package hqizcc.pojo;
 public class User {
     private String username;
 
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     public String getUsername() {
         return username;
     }
